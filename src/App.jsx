84000f628fe837be4030a25be1af965571e8973b
Divesh Nikam <divesh.nikam@gmail.com>
2025-12-28 12:09:16 +0530

    fix: configure bootstrap imports and add vercel rewrite

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { useState } from "react";
 import StoryBar from "./components/StoryBar";
 import stories from "./data/stories";
